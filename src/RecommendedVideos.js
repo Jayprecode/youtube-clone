c20ff37415439b040fc91adf3d18a9712c4aca22
Jayprecode <jayprecode@gmail.com>
2021-01-13 10:48:08 +0100

    initial commit

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -4,26 +4,105 @@ import VideoCard from "./VideoCard";
 
 function RecommendedVideos() {
     return (
-      <div className="RecommendedVideos">
+      <div className="recommendedVideos">
         <h2>Recommended</h2>
         <div className="recommendedVideos__videos">
           <VideoCard
-            title=""
-            views=""
-            timestamp=""
-            channelImage=""
-            channel=""
-            image=""
+            title="PS5 : HOW TO DESIGN A PS5 CONTROLLER USING HTML & CSS -THE NEW DUALSENSE PLAYSTATION 5 CONTROLLER 🎮"
+            views="10M views"
+            timestamp="1 year ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/jPdWx862o9U/mqdefault_6s.webp?du=3000&sqp=CJq--v8F&rs=AOn4CLAJrZbWIT0JsXLmBZxf4FHsfcZubw"
           />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
+          <VideoCard
+            title="Sticky Navigation Bar On Responsive Scroll with Hover Effect Using CSS & JS - Fixed navbar on scroll"
+            views="4M views"
+            timestamp="3 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/Altx6NzYfhI/mqdefault_6s.webp?du=3000&sqp=CKjG-v8F&rs=AOn4CLA3rsCuZCdobhQYgMWBgdkq3I9Pfg"
+          />
+          <VideoCard
+            title="How Advanced Animated Navigation Menu Bar Are Created Using HTML, CSS & JAVASCRIPT -Don't skip this"
+            views="6M views"
+            timestamp="6 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/b_yDLHuxu-s/mqdefault_6s.webp?du=3000&sqp=CI29-v8F&rs=AOn4CLAXKQNzKQC6HRB_NOjyQrxYHKdjNA"
+          />
+          <VideoCard
+            title="How To Make Drop Down Menu Using HTML, CSS & JS For Absolute beginner"
+            views="3M views"
+            timestamp="7 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/F27-PJHR538/mqdefault_6s.webp?du=3000&sqp=COfG-v8F&rs=AOn4CLB5GhjqbSr_epeqtS1DhYgM492uQg"
+          />
+          <VideoCard
+            title="Full Screen Video Background Using HTML & CSS | How To Add a Video Background With HTML & CSS"
+            views="5M views"
+            timestamp="8 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/ih7oILhf66U/mqdefault_6s.webp?du=3000&sqp=CMTE-v8F&rs=AOn4CLDcutu3LwgAcdaKA346rxdor7W-hA"
+          />
+          <VideoCard
+            title="2 Simple & Quickest Way to Center Text or DIV with CSS In 2020 For Beginners - CSS Center text"
+            views="1M views"
+            timestamp="9 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/tduRRjViZT0/mqdefault_6s.webp?du=3000&sqp=CIG6-v8F&rs=AOn4CLBwy5V9GqYHLgLEE5sgBzkxai_-XA"
+          />
+          <VideoCard
+            title="PS5 : HOW TO DESIGN A PS5 CONTROLLER USING HTML & CSS -THE NEW DUALSENSE PLAYSTATION 5 CONTROLLER 🎮"
+            views="10M views"
+            timestamp="1 year ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/jPdWx862o9U/mqdefault_6s.webp?du=3000&sqp=CJq--v8F&rs=AOn4CLAJrZbWIT0JsXLmBZxf4FHsfcZubw"
+          />
+          <VideoCard
+            title="Sticky Navigation Bar On Responsive Scroll with Hover Effect Using CSS & JS - Fixed navbar on scroll"
+            views="4M views"
+            timestamp="3 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/Altx6NzYfhI/mqdefault_6s.webp?du=3000&sqp=CKjG-v8F&rs=AOn4CLA3rsCuZCdobhQYgMWBgdkq3I9Pfg"
+          />
+          <VideoCard
+            title="How Advanced Animated Navigation Menu Bar Are Created Using HTML, CSS & JAVASCRIPT -Don't skip this"
+            views="6M views"
+            timestamp="6 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/b_yDLHuxu-s/mqdefault_6s.webp?du=3000&sqp=CI29-v8F&rs=AOn4CLAXKQNzKQC6HRB_NOjyQrxYHKdjNA"
+          />
+          <VideoCard
+            title="How To Make Drop Down Menu Using HTML, CSS & JS For Absolute beginner"
+            views="3M views"
+            timestamp="7 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/F27-PJHR538/mqdefault_6s.webp?du=3000&sqp=COfG-v8F&rs=AOn4CLB5GhjqbSr_epeqtS1DhYgM492uQg"
+          />
+          <VideoCard
+            title="Full Screen Video Background Using HTML & CSS | How To Add a Video Background With HTML & CSS"
+            views="5M views"
+            timestamp="8 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/ih7oILhf66U/mqdefault_6s.webp?du=3000&sqp=CMTE-v8F&rs=AOn4CLDcutu3LwgAcdaKA346rxdor7W-hA"
+          />
+          <VideoCard
+            title="2 Simple & Quickest Way to Center Text or DIV with CSS In 2020 For Beginners - CSS Center text"
+            views="1M views"
+            timestamp="9 months ago"
+            channelImage="https://yt3.ggpht.com/ytc/AAUvwngY4rUgMvKwRdw_MmBQXvX_BEq8FGEx04_rQBQiXw=s48-c-k-c0xffffffff-no-rj-mo"
+            channel="Jayprecode"
+            image="https://i.ytimg.com/an_webp/tduRRjViZT0/mqdefault_6s.webp?du=3000&sqp=CIG6-v8F&rs=AOn4CLBwy5V9GqYHLgLEE5sgBzkxai_-XA"
+          />
         </div>
       </div>
     );
