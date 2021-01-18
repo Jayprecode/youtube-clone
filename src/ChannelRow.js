41529ce5b7da9bd83630ba9204599eddaa6e643a
Jayprecode <jayprecode@gmail.com>
2021-01-18 13:21:03 +0100

    search function

---
+++ src/ChannelRow.js
@@ -0,0 +1,24 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./ChannelRow.css";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
+
+
+function ChannelRow({image, channel, subs, no0fVideos, description, verified}) {
+    return (
+      <div className="channelRow">
+        <Avatar className="channelRow__logo" alt={channel} src={image} />
+        <div className="channelRow__text">
+          <h4>
+            {channel} {verified && <VerifiedIcon />}
+          </h4>
+          <p>
+            {subs} subscriber • {no0fVideos} videos
+          </p>
+          <p>{description}</p>
+        </div>
+      </div>
+    ); 
+}
+
+export default ChannelRow
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,12 +1,86 @@
+import { TuneOutlined } from "@material-ui/icons";
 import React from "react";
 import "./SearchPage.css";
+import CI from "./img/CI.png"
+import AL from "./img/AL.jpg"
+import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 
 function SearchPage() {
     return (
-        <div className="searchPage">
-            <h2>I am here</h2>
+      <div className="searchPage">
+        <div className="searchPage_box">
+          <div className="searchPage__filter">
+            <TuneOutlined />
+            <h2>FILTER</h2>
+          </div>
+          <hr />
+          <ChannelRow
+            image={CI}
+            channel="Jayprecode"
+            verified
+            subs="660k"
+            noOfVideos={382}
+            description="Jayprecode is a platform for learning various computer technologies and programming languages. 100% FREE TIPS & TRICKS,..."
+          />
+          <hr />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
+          <VideoRow
+            views="1.4M"
+            subs="659k"
+            description="Do you want a FREE one hour training..check"
+            timestamp="59 seconds ago"
+            channel="Jayprecode"
+            title="Animated Login Form Using HTML & CSS & JQuery - HOW TO Create an Awesome Animated Login Form in 2020"
+            image={AL}
+          />
         </div>
-    )
+      </div>
+    );
 }
 
 export default SearchPage
